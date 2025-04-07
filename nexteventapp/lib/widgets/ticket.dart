59f3615b3fd5a6c35cf073dfdfaf0e8a2c9066fb
Kareem Khater <118448251+AnaaaKareem@users.noreply.github.com>
2025-04-07 17:56:33 +0000

    Added the ticket widget

---
+++ nexteventapp/lib/widgets/ticket.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class Ticket extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: 500,
+        padding: EdgeInsets.all(30),
+        margin: EdgeInsets.all(30),
+        decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade300,
+            blurRadius: 10,
+            offset: Offset(0, 4),
+          ),
+        ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            QrImageView(data: 'https://findtheinvisiblecow.com/',
+            version: QrVersions.auto,
+            size: 300.0),
+            Container(
+              width: 280,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text('Event Name'),
+                  Text('Organizers'),
+                  Row(
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Name: '),
+                      Text('Kareem')
+                    ],
+                  ),
+                  Text('Date and Time'),
+                  Row(
+                    children: [
+                      Text('Building '),
+                      Text('Twin Towers'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text('Room '),
+                      Text('100'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
